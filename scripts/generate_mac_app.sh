0083d7d8eecac19c1bcb8058de233f4e58bbcfdf
Nightmare-MY <906262255@qq.com>
2021-05-05 18:45:38 +0800

    Update generate_mac_app.sh

--- scripts/generate_mac_app.sh
+++ scripts/generate_mac_app.sh
@@ -1,7 +1,5 @@
 flutter build macos
 rm -rf ./build/macos/Build/Products/Release/移动到这
 ln -s -f /Applications ./build/macos/Build/Products/Release/移动到这
-mkdir -p ./build/macos/Build/Products/Release/Termare.app/Contents/MacOS/data/usr/bin
-cp ./executable/macos/adb ./build/macos/Build/Products/Release/Termare.app/Contents/MacOS/data/usr/bin
-tar -zcvf ./remote.tar  -C ./build/macos/Build/Products/  Release/Termare.app/ Release/移动到这
+tar -zcvf ./Termare.tar  -C ./build/macos/Build/Products/  Release/Termare.app/ Release/移动到这
 rm -rf ./build/macos/Build/Products/Release/移动到这
